71a3ef5a1728f83b26f6830159e1ab7f90d23081
Sacha Castells <sc@castells.de>
2023-03-10 11:47:44 +0100

    add mfe support

--- demo-shell/apps/demo-app/src/app/app.routes.ts
+++ demo-shell/apps/demo-app/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Route } from '@angular/router';
-import {NxWelcomeComponent} from "./nx-welcome.component";
-import {I18NEXT_NAMESPACE_RESOLVER} from "angular-i18next";
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { I18NEXT_NAMESPACE_RESOLVER } from 'angular-i18next';
 
 export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+  },
   {
     path: '',
     children: [
